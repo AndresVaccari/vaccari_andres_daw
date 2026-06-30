6a2ba03ba6705fea8869254f8a285b680ef3b39c
Andrés Juan Vaccari <93448866+AndresVaccari@users.noreply.github.com>
2026-06-30 20:37:45 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,22 +5,24 @@ const boton = document.getElementById("btnClima")
 
 async function getClima () {
     try {
+        if (ciudad.value.trim() == "") {
+            estado.textContent = "Ingrese una ciudad"
+            return
+        }
+
         boton.disable
 
         estado.textContent = "Cargando"
-        const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${ciudad.value}&count=1`).then((r) => {
-            console.log("r", r)
-        })
+        const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${ciudad.value}&count=1`)
+        const data = await response.json()
+        const results = data.results
 
-        const data = response
+        const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${results[0].latitude}&longitude=${results[0].longitude}&current_weather=true`)
+        const weatherData = await weatherResponse.json()
 
-        console.log("response", response)
-        console.log("data", data)
-
-        estado.textContent = "Clima Cargado"
+        estado.textContent = `${ciudad.value} -- Temperatura ${weatherData.current_weather.temperature} -- Viento ${weatherData.current_weather.windspeed} -- Codigo Clima ${weatherData.current_weather.weathercode}`
     } catch (e) {
-        console(e)
-        estado.textContent = "Error"
+        estado.textContent = `Ocurrio un error ${e}`
     } finally {
         boton.enable
     }
